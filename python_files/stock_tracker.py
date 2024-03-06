0b3ab34029ad323fb8795d1e74521f0130706b0e
mjmacarty <matt.macarty@gmail.com>
2024-03-06 12:03:58 -0500

    class files

--- python_files/stock_tracker.py
+++ python_files/stock_tracker.py
@@ -1,11 +1,55 @@
 from time import sleep
+import sys
 import yfinance as yf
 
 
-def main():
+def display_menu():
+    print("""
+    1. Track Watchlist
+    2. Add Watchlist
+    3. Edit Watchlist
+    4. Delete Watchlist
+    5. Exit
+    """)
+
+def track(watchlist):
     for second in range(10):
         print(f"{yf.Ticker('AAPL').history()['Close'][-1]:.2f}")
         sleep(1)
+
+
+def add_list():
+    #TODO write adding list functionality
+    print("Add a new list!")
+
+
+def edit_list():
+    #TODO write logic for adding/deleteing from list
+    print("Edit a list!")
+
+
+def delete():
+    #TODO write logic for deleting a list
+    print("Delete a list!")
+
+
+
+actions = {'2': add_list, '3': edit_list, '4': delete}
+def main():
+    print("Welcome to StockTraker!")
+    while True:
+        watchlist = 'AMZN NFLX FB GOOG'.split()
+        display_menu()
+        choice = input("Enter a menu number: ")
+        if choice == '1':
+            track(watchlist)
+        elif choice in '234':
+            actions[choice]()
+        elif choice == '5':
+            print("Goodbye!")
+            sys.exit()
+        else:
+            print("Enter a valid menu number!")
 
 if __name__ == "__main__":
     main()
---
+++ python_files/stock_tracker_functions.py
@@ -0,0 +1,29 @@
+import os
+from time import sleep, time
+import yfinance as yf
+# track list
+"""
+1. allow continous tracking of a list of stocks
+2. allow user to choose which list 
+"""
+watchlist = 'AMZN NFLX META GOOG'.split()
+def track(watchlist):
+    start = time()
+    while True:
+        for symbol in watchlist:
+            print(f"{symbol:8}{yf.Ticker(symbol).history()['Close'][-1]:.2f}")
+            sleep(1)
+        if time() - start >= 15:
+            prompt = input("Enter to continue, any key to quit: ")
+            start = time()
+            if prompt:
+                break
+def read_directory():
+    if not os.path.exists('watchlists'):
+        print("No watchlist directory, creating")
+        os.mkdir('watchlists')
+
+read_directory()
+print(os.listdir('watchlists'))
+
+#track(watchlist)
